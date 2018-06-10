b5e3627e56e0b79f6cec5c294b2f81ae22b052c1
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2018-06-10 22:36:01 +0300

    layout main page (not sliders)

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-   './bower_components/Snap.svg/dist/snap.svg-min.js'
+   './bower_components/Snap.svg/dist/snap.svg-min.js',
+   './node_modules/../source/js/swiper.js'
 ];
