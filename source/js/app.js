9ffa4fd84e061cf8f8b07ca6c44b12de57b0e7e8
isTerra-maksYgso <maks.koledyuk@gmail.com>
2018-07-19 21:51:11 +0300

    update 19.07.2018

--- source/js/app.js
+++ source/js/app.js
@@ -3,11 +3,42 @@ var $ = document;
 
 window.onload = () => {
    openlist($.querySelector('.header-contacts__select-check'), $.querySelector('.header-contacts__select-list'));
-   phoneMask($.querySelector('.question-form').querySelector('input[name=phone]'));
+   phoneMask($.querySelector('.question-form input[name=phone]'));
    altMenu();
    modalFunction();
    swiper();
+   // $.querySelectorAll('.bg-circle').forEach(item => {
+   //    t(item);
+   // })
 };
+
+// var t = el => {
+//    var boxercontainer = $.querySelector('.main');
+//    const boxer = el,
+//       // maxMove = boxercontainer.offsetWidth / 30,
+//       maxMove = 200,
+//       boxerCenterX = boxer.offsetLeft + (boxer.offsetWidth / 2),
+//       boxerCenterY = boxer.offsetTop + (boxer.offsetHeight / 2),
+//       fluidboxer = window.matchMedia("(min-width: 726px)");
+
+//    var getMousePos = (xRef, yRef) => {
+//       let panelRect = boxercontainer.getBoundingClientRect();
+//       return {
+//          x: Math.floor(xRef - panelRect.left) /(panelRect.right-panelRect.left)*boxercontainer.offsetWidth,
+//          y: Math.floor(yRef - panelRect.top) / (panelRect.bottom -panelRect.top) * boxercontainer.offsetHeight
+//       };
+//    }
+
+//    $.body.addEventListener("mousemove", function(e) {
+//       let mousePos = getMousePos(e.clientX, e.clientY),
+//       distX = mousePos.x - boxerCenterX,
+//       distY = mousePos.y - boxerCenterY;
+//       if (Math.abs(distX) < 500 && distY < 200 && fluidboxer.matches) {
+//       boxer.style.transform = "translate("+(-1*distX)/12+"px,"+(-1*distY)/12+"px)";
+//          boxercontainer.style.backgroundPosition = `calc(50% + ${distX/50}px) calc(50% + ${distY/50}px)`;
+//       }
+//    });
+// }
 
 var altMenu = () => {
    var ctrl = $.querySelector('.alt-menu-btn');
@@ -18,6 +49,7 @@ var altMenu = () => {
 };
 
 var phoneMask = item => {
+   if (!item) return false;
    MaskedInput({
       elm: item,
       format: item.value,
